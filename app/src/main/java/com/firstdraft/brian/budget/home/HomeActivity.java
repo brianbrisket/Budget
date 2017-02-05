00e08ee9279e45e6c8264561946721f652f99826
brianbrisket <brian.brisket@gmail.com>
2017-02-05 13:32:51 -0800

    Add home activity and button to access expense view activity

---
+++ app/src/main/java/com/firstdraft/brian/budget/home/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.firstdraft.brian.budget.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.firstdraft.brian.budget.R;
+import com.firstdraft.brian.budget.expense.ExpenseViewActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    private Button addExpenseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        addExpenseButton = (Button) findViewById(R.id.home_add_expense_button);
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), ExpenseViewActivity.class));
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/firstdraft/brian/budget/home/HomeContract.java
@@ -0,0 +1,10 @@
+package com.firstdraft.brian.budget.home;
+
+/**
+ * Created by brian on 2/5/2017.
+ */
+
+public interface HomeContract {
+
+
+}
