f8a6c71a8083af9e6c49291bbdbd943b838261e5
brianbrisket <brian.brisket@gmail.com>
2017-02-05 11:53:10 -0800

    Add implementation for Monetary Input, Recurrence, Category and Save Expense in ExpenseView Use appTheme for monetary input Add string arrays for recurrence and categories

--- app/src/main/java/com/firstdraft/brian/budget/expense/ExpenseViewActivity.java
+++ app/src/main/java/com/firstdraft/brian/budget/expense/ExpenseViewActivity.java
@@ -1,8 +1,9 @@
 package com.firstdraft.brian.budget.expense;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -46,28 +47,77 @@ public class ExpenseViewActivity extends AppCompatActivity implements ExpenseCon
         presenter.bindView(this);
     }
 
-    // TODO: 2/2/2017 create presenter in the oncreate method and do the bind view and implement the flow
+    // TODO: 2/4/2017 write test to check against different currency locales
+    // TODO: 2/4/2017 deselect input field on button click
+    // TODO: 2/4/2017 change button text on edittext select
+    // TODO: 2/4/2017 make button layout relative above the number pad to give user option to confirm
     @Override
     public void showMonetaryInputField() {
         expenseInputTitle.setVisibility(View.VISIBLE);
         expenseInputField.setVisibility(View.VISIBLE);
+        // TODO: 2/4/2017 change name of add expense button to something more relevant
+        addExpenseButton.setVisibility(View.VISIBLE);
+        // TODO: 2/4/2017 add to strings.xml "saveMonetaryInputFieldTextonButton" or something like that
+        addExpenseButton.setText("Confirm Amount");
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currencySymbol = expenseInputField.getCurrency().getSymbol();
+                String expenseValueWithoutSymbol = expenseInputField.getText().toString()
+                        .replace(currencySymbol, "");
+                presenter.monetaryInputReceived(Float.parseFloat(expenseValueWithoutSymbol));
+            }
+        });
     }
 
+    // TODO: 2/4/2017 consider putting in a first time ux option to show "Select from below"
     @Override
     public void showRecurrenceField() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_recurrence_values, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        recurrenceMenu.setAdapter(adapter);
+
         recurrenceMenuTitle.setVisibility(View.VISIBLE);
         recurrenceMenu.setVisibility(View.VISIBLE);
+
+        addExpenseButton.setText("Confirm Recurrence");
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.recurrenceSelected(recurrenceMenu.getSelectedItem().toString());
+            }
+        });
     }
 
     @Override
     public void showCategoryMenu(String[] list) {
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoryMenu.setAdapter(adapter);
+
         categoryMenuTitle.setVisibility(View.VISIBLE);
         categoryMenu.setVisibility(View.VISIBLE);
 
+        addExpenseButton.setText("Confirm Category");
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.categorySelected(categoryMenu.getSelectedItem().toString());
+            }
+        });
     }
 
     @Override
     public void showConfirmExpenseButton() {
+        addExpenseButton.setText("Save Expense");
         addExpenseButton.setVisibility(View.VISIBLE);
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.expenseButtonClicked();
+            }
+        });
     }
 }
