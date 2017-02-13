81431f6c82775a2cec526c38837781e3a99cb01e
brianbrisket <brian.brisket@gmail.com>
2017-02-13 12:39:33 -0800

    Add income view layout file

---
+++ app/src/main/java/com/firstdraft/brian/budget/income/IncomeViewActivity.java
@@ -0,0 +1,49 @@
+package com.firstdraft.brian.budget.income;
+
+/**
+ * Created by brisket on 2/13/2017.
+ */
+public class IncomeViewActivity implements IncomeContract.View {
+
+
+
+    @Override
+    public void showMonetaryInputField() {
+
+    }
+
+    @Override
+    public void showRecurrenceSpinner() {
+
+    }
+
+    @Override
+    public void showCategoriesSpinner(String[] list) {
+
+    }
+
+    @Override
+    public void showConfirmIncomeButton() {
+
+    }
+
+    @Override
+    public void showLoadingView() {
+
+    }
+
+    @Override
+    public void dismissLoadingView() {
+
+    }
+
+    @Override
+    public void showErrorView(String errorMessage) {
+
+    }
+
+    @Override
+    public void showIncomeSavedView() {
+
+    }
+}
